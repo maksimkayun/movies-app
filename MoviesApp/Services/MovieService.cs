58e31b54505f97212f222c2513fad9170e48e0ee
maksk <maks.kayun@yandex.ru>
2021-12-09 11:19:09 +0300

    fix edit confirmed

--- MoviesApp/Services/MovieService.cs
+++ MoviesApp/Services/MovieService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesApp.Controllers;
 using MoviesApp.Data;
